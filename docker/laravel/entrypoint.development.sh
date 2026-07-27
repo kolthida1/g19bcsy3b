f84d75595592d752483eeee37c9000fd5f37f06d
daemda1 <daemda1@gmail.com>
2026-07-27 08:59:27 +0700

    Session 3.7 Laravel Octane + RoadRunner development environment

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -6,4 +6,8 @@ php artisan key:generate
 wait $!
 php artisan migrate
 wait $!
-php artisan serve --host=0.0.0.0 --port=8000
+#php artisan serve --host=0.0.0.0 --port=8000 &
+
+#php artisan queue:work --tries=3 
+
+exec supervisord -c /etc/supervisor/conf.d/supervisord.development.conf
